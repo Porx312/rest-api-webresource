863adc220aa0dd030296d28f3422588763017c40
porx <105926826+Porx312@users.noreply.github.com>
2024-04-03 16:49:02 +0200

    add

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express') // require -> commonJS
 const crypto = require('node:crypto')
 const cors = require('cors')
 
-const movies = require('./Resources.json')
+const movies = require('./movies.json')
 const { validateMovie, validatePartialMovie } = require('./schemas/movies')
 
 const app = express()
