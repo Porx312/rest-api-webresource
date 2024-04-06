665310befcf0c215ff7cfcb5a9d1cdc493e7a2b7
porx <105926826+Porx312@users.noreply.github.com>
2024-04-06 19:14:45 +0200

    sd

--- index.js
+++ index.js
@@ -21,15 +21,12 @@ const writeData = async (data) => {
 
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-    const data = await readData();
-    res.json(data);
-});
+// Ruta para obtener un elemento por su id en una sección específica
 app.get("/:section/:id", async (req, res) => {
     const { section, id } = req.params;
     const data = await readData();
     const sectionData = data[section]; // Acceder a la sección correspondiente
-    const item = sectionData.find(item => item.id === parseInt(id)); // Buscar el elemento por nombre
+    const item = sectionData.find(item => item.id === parseInt(id)); // Buscar el elemento por id
     if (item) {
         res.json(item);
     } else {
@@ -37,19 +34,7 @@ app.get("/:section/:id", async (req, res) => {
     }
 });
 
-app.get("/:query", async (req, res) => {
-    const { query } = req.params;
-    const data = await readData();
-    const allItems = Object.values(data).flat(); // Obtener todos los elementos de todas las secciones
-    const regex = new RegExp(query, 'i'); // Crear una expresión regular que coincida con cualquier parte de la cadena
-    const matchingItems = allItems.filter(item => regex.test(item.nombre)); // Filtrar los elementos que coincidan con la consulta
-    if (matchingItems.length > 0) {
-        res.json(matchingItems);
-    } else {
-        res.status(404).json({ message: 'No matching items found' });
-    }
-});
-// Ruta para obtener la sesión completa
+// Ruta para obtener la sesión completa de una sección
 app.get("/:section", async (req, res) => {
     const { section } = req.params;
     const data = await readData();
@@ -61,6 +46,25 @@ app.get("/:section", async (req, res) => {
     }
 });
 
+// Ruta para realizar una búsqueda flexible por nombre
+app.get("/search/:query", async (req, res) => {
+    const { query } = req.params;
+    const data = await readData();
+    const allItems = Object.values(data).flat(); // Obtener todos los elementos de todas las secciones
+    const regex = new RegExp(query, 'i'); // Crear una expresión regular que coincida con cualquier parte de la cadena
+    const matchingItems = allItems.filter(item => regex.test(item.nombre)); // Filtrar los elementos que coincidan con la consulta
+    if (matchingItems.length > 0) {
+        res.json(matchingItems);
+    } else {
+        res.status(404).json({ message: 'No matching items found' });
+    }
+});
+
+// Ruta para obtener todas las secciones
+app.get("/", async (req, res) => {
+    const data = await readData();
+    res.json(data);
+});
 
 const PORT = process.env.PORT || 3000; // Usar el puerto proporcionado por el servicio de hosting o 3000 por defecto
 
