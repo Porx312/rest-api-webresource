e9d1f7be04df254e7adb79643f8095d0163e844e
porx <105926826+Porx312@users.noreply.github.com>
2024-04-22 16:50:02 +0200

    api corregida

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express";
+import cors from "cors";
 import fs from "fs/promises";
+
 const app = express();
+app.use(cors());
 
 const readData = async () => {
   try {
@@ -8,14 +11,7 @@ const readData = async () => {
     return JSON.parse(data);
   } catch (err) {
     console.log(err);
-  }
-};
-
-const writeData = async (data) => {
-  try {
-    await fs.writeFile("./db.json", JSON.stringify(data));
-  } catch (err) {
-    console.log(err);
+    return null; // Devuelve null en caso de error
   }
 };
 
@@ -25,12 +21,16 @@ app.use(express.json());
 app.get("/:section/:id", async (req, res) => {
   const { section, id } = req.params;
   const data = await readData();
-  const sectionData = data[section]; // Acceder a la sección correspondiente
-  const item = sectionData.find((item) => item.id === parseInt(id)); // Buscar el elemento por id
-  if (item) {
-    res.json(item);
+  if (data) {
+    const sectionData = data[section]; // Acceder a la sección correspondiente
+    const item = sectionData.find((item) => item.id === parseInt(id)); // Buscar el elemento por id
+    if (item) {
+      res.json(item);
+    } else {
+      res.status(404).json({ message: "Item not found" });
+    }
   } else {
-    res.status(404).json({ message: "Item not found" });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
@@ -38,11 +38,15 @@ app.get("/:section/:id", async (req, res) => {
 app.get("/:section", async (req, res) => {
   const { section } = req.params;
   const data = await readData();
-  const sectionData = data[section]; // Acceder a la sección correspondiente
-  if (sectionData) {
-    res.json(sectionData);
+  if (data) {
+    const sectionData = data[section]; // Acceder a la sección correspondiente
+    if (sectionData) {
+      res.json(sectionData);
+    } else {
+      res.status(404).json({ message: "Section not found" });
+    }
   } else {
-    res.status(404).json({ message: "Section not found" });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
@@ -50,23 +54,31 @@ app.get("/:section", async (req, res) => {
 app.get("/search/:query", async (req, res) => {
   const { query } = req.params;
   const data = await readData();
-  const allItems = Object.values(data).flat(); // Obtener todos los elementos de todas las secciones
-  const regex = new RegExp(query, "i"); // Crear una expresión regular que coincida con cualquier parte de la cadena
-  const matchingItems = allItems.filter((item) => regex.test(item.nombre)); // Filtrar los elementos que coincidan con la consulta
-  if (matchingItems.length > 0) {
-    res.json(matchingItems);
+  if (data) {
+    const allItems = Object.values(data).flat(); // Obtener todos los elementos de todas las secciones
+    const regex = new RegExp(query, "i"); // Crear una expresión regular que coincida con cualquier parte de la cadena
+    const matchingItems = allItems.filter((item) => regex.test(item.nombre)); // Filtrar los elementos que coincidan con la consulta
+    if (matchingItems.length > 0) {
+      res.json(matchingItems);
+    } else {
+      res.status(404).json({ message: "No matching items found" });
+    }
   } else {
-    res.status(404).json({ message: "No matching items found" });
+    res.status(500).json({ message: "Internal server error" });
   }
 });
 
 // Ruta para obtener todas las secciones
 app.get("/", async (req, res) => {
   const data = await readData();
-  res.json(data);
+  if (data) {
+    res.json(data);
+  } else {
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
-const PORT = process.env.PORT || 3000; // Usar el puerto proporcionado por el servicio de hosting o 3000 por defecto
+const PORT = process.env.PORT || 5000; // Usar el puerto proporcionado por el servicio de hosting o 3000 por defecto
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
